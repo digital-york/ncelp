eb12b82faf651ed336096aa61c01f7acc76594bb
Frank Feng <y.frank.feng@gmail.com>
2019-02-01 10:35:10 +0000

    add language

--- app/forms/hyrax/resource_form.rb
+++ app/forms/hyrax/resource_form.rb
@@ -4,18 +4,20 @@ module Hyrax
   # Generated form for Resource
   class ResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::Resource
-    self.terms = [:title,
-                  :creator,
+    self.terms = [:title,                # title from default Hyrax metadata
+                  :creator,              # creator from default Hyrax
                   :affiliation,
                   :one_line_description,
-                  :full_description
+                  :full_description,
+                  :language              # language from default Hyrax
                  ]
 
     self.required_fields = [
                   :title,
                   :creator,
                   :one_line_description,
-                  :full_description
+                  :full_description,
+                  :language
                  ]
   end
 end
