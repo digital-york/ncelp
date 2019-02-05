f752423b7f1249b2780f85b84d109353961c9b30
Frank Feng <y.frank.feng@gmail.com>
2019-02-05 11:49:03 +0000

    defined customized facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -41,17 +41,16 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
-    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
-    config.add_facet_field solr_name("creator", :facetable), limit: 5
-    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
-    config.add_facet_field solr_name("keyword", :facetable), limit: 5
-    config.add_facet_field solr_name("subject", :facetable), limit: 5
     config.add_facet_field solr_name("language", :facetable), limit: 5
-    config.add_facet_field solr_name("based_near_label", :facetable), limit: 5
-    config.add_facet_field solr_name("publisher", :facetable), limit: 5
-    config.add_facet_field solr_name("file_format", :facetable), limit: 5
-    config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', helper_method: :collection_title_by_id
+    config.add_facet_field solr_name("type_of_material", :facetable), label: "Type of material", limit: 5
+    config.add_facet_field solr_name("modality", :facetable), label: "Modality / mode", limit: 5
+    config.add_facet_field solr_name("area_of_research", :facetable), label: "Area of research", limit: 5
+    config.add_facet_field solr_name("age", :facetable), label: "Age", limit: 5
+    config.add_facet_field solr_name("exposure", :facetable), label: "Exposure", limit: 5
+    config.add_facet_field solr_name("file_type", :facetable), label: "File", limit: 5
+    config.add_facet_field solr_name("country", :facetable), limit: 5
+    config.add_facet_field solr_name("creator", :facetable), limit: 5
+    config.add_facet_field solr_name("reference", :facetable), label: "Original material", limit: 5
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
