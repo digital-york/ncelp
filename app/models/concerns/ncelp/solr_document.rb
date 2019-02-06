01136fa7051462882d87a5ca585247fed8f8c95a
Frank Feng <y.frank.feng@gmail.com>
2019-02-06 15:08:41 +0000

    define customized solr_document

---
+++ app/models/concerns/ncelp/solr_document.rb
@@ -0,0 +1,58 @@
+module Ncelp
+  module SolrDocument extend ActiveSupport::Concern
+    module Solr
+      class Array
+        # @return [Array]
+        def self.coerce(input)
+          ::Array.wrap(input)
+        end
+      end
+
+      class String
+        # @return [String]
+        def self.coerce(input)
+          ::Array.wrap(input).first
+        end
+      end
+
+      class Date
+        # @return [Date]
+        def self.coerce(input)
+          field = String.coerce(input)
+          return if field.blank?
+          begin
+            ::Date.parse(field)
+          rescue ArgumentError
+            Rails.logger.info "Unable to parse date: #{field.first.inspect}"
+          end
+        end
+      end
+    end
+
+    included do
+      attribute :title,                    Solr::Array,  solr_name('title')
+      attribute :creator,                  Solr::Array,  solr_name('creator')
+      attribute :affiliation,              Solr::Array,  solr_name('affiliation')
+      attribute :one_line_description,     Solr::String, solr_name('one_line_description')
+      attribute :full_description,         Solr::String, solr_name('full_description')
+      attribute :language,                 Solr::String, solr_name('language')
+      attribute :type_of_material,         Solr::String, solr_name('type_of_material')
+      attribute :topic,                    Solr::Array,  solr_name('topic')
+      attribute :area_of_research,         Solr::Array,  solr_name('area_of_research')
+      attribute :modality,                 Solr::String, solr_name('modality')
+      attribute :age,                      Solr::Array,  solr_name('age')
+      attribute :exposure,                 Solr::String, solr_name('exposure')
+      attribute :file_type,                Solr::Array,  solr_name('file_type')
+      attribute :cty,                      Solr::String, solr_name('country')
+      attribute :ctyother,                 Solr::String, solr_name('country_other')
+      attribute :link_to_oasis,            Solr::Array,  solr_name('link_to_oasis')
+      attribute :link_to_iris,             Solr::Array,  solr_name('link_to_iris')
+      attribute :link_to_video,            Solr::Array,  solr_name('link_to_video')
+      attribute :reference,                Solr::Array,  solr_name('reference')
+      attribute :terms_and_conditions,     Solr::String, solr_name('terms_and_conditions')
+      attribute :license,                  Solr::String, solr_name('license')
+      attribute :email,                    Solr::String, solr_name('email')
+      attribute :notes,                    Solr::String, solr_name('notes')
+    end
+  end
+end
--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -5,7 +5,7 @@ class SolrDocument
 
   # Adds Hyrax behaviors to the SolrDocument.
   include Hyrax::SolrDocumentBehavior
-
+  include Ncelp::SolrDocument
 
   # self.unique_key = 'id'
 
