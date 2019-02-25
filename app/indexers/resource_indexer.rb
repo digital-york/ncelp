c6ce5bcf5b86aa6a965b8435c64ad50af16921ec
Frank Feng <y.frank.feng@gmail.com>
2019-02-25 15:18:39 +0000

    get customized indexer working

--- app/indexers/resource_indexer.rb
+++ app/indexers/resource_indexer.rb
@@ -9,10 +9,41 @@ class ResourceIndexer < Hyrax::WorkIndexer
   # this behavior
   include Hyrax::IndexesLinkedMetadata
 
-  # Uncomment this block if you want to add custom indexing behavior:
-  # def generate_solr_document
-  #  super.tap do |solr_doc|
-  #    solr_doc['my_custom_field_ssim'] = object.my_custom_property
-  #  end
-  # end
+  # add custom indexing behavior for authority fields:
+  def generate_solr_document
+    super.tap do |solr_doc|
+      # add authority labels into solr
+      NcelpAuthorities.authority_list.each do |field_name|
+        authority_filename = "#{field_name}.yml"
+        begin
+          unless object[field_name].nil?
+            if object[field_name].is_a? String and object[field_name]!=''
+              if object[field_name].starts_with? 'http' or object[field_name].to_i <= 0
+                solr_doc[field_name+'_label_tesim'] = GenericLocalAuthorityService.id_to_label(field_name, object[field_name])
+              else
+                solr_doc[field_name+'_label_tesim'] = GenericLocalAuthorityService.id_to_label(field_name, object[field_name].to_i)
+              end
+            elsif object[field_name].size>0
+              labels = []
+              object[field_name].each do |id|
+                unless (id.nil? or id=='')
+                  if id.starts_with? 'http' or id.to_i <= 0
+                    labels << GenericLocalAuthorityService.id_to_label(field_name, id)
+                  else
+                    labels << GenericLocalAuthorityService.id_to_label(field_name, id.to_i)
+                  end
+                end
+              end
+              solr_doc[field_name+'_label_tesim'] = labels
+            else
+              Rails.logger.info('ignored empty field: ' + field_name)
+            end
+          end
+        rescue => exception
+          Rails.logger.error('Failed while indexing label for ' + field_name)
+          puts exception.backtrace
+        end
+      end
+    end
+  end
 end
---
+++ app/models/ncelp_authorities.rb
@@ -0,0 +1,5 @@
+class NcelpAuthorities
+    def self.authority_list
+        %w[age area_of_research exposure file_type language material_for_teachers modality thematic topic type_of_material]
+    end
+end
