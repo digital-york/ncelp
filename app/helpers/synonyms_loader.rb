852b7b0a1bf5cbb88ff0aed2e362c201a4954d47
Frank Feng <y.frank.feng@gmail.com>
2019-02-28 10:34:47 +0000

    add comments

--- app/helpers/synonyms_loader.rb
+++ app/helpers/synonyms_loader.rb
@@ -3,6 +3,8 @@ require 'yaml'
 class SynonymsLoader
     @@synonym_path = 'config/authorities/'
 
+    # return an Array containing all synonyms for a term in a particular authority
+    # return nil if cannot find any
     def self.load_synonyms(authority_name, term_id)
         begin
             synonyms = YAML.load_file("#{@@synonym_path}#{authority_name}_synonyms.yml")
