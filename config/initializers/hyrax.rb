2dddcbe38cf5cf840e304e28032d2fdbc08364f3
Frank Feng <y.frank.feng@gmail.com>
2019-03-12 14:14:57 +0000

    load fits path from Figaro

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -86,6 +86,7 @@ Hyrax.config do |config|
 
   # Path to the file characterization tool
   # config.fits_path = "fits.sh"
+  config.fits_path = Figaro.env.fits_path
 
   # Path to the file derivatives creation tool
   # config.libreoffice_path = "soffice"
