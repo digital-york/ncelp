6ab658cef1732185158b715bb42c51720f563ccf
Frank Feng <y.frank.feng@gmail.com>
2019-03-13 10:33:11 +0000

    add survey controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,4 +1,23 @@
 class SurveyController < ApplicationController
-  def new
+  protect_from_forgery
+
+  with_themed_layout 'survey'
+
+  def new; end
+
+  def submit
+    resource_id         = params['resource_id']
+    resource_fileset_id = params['resource_fileset_id']
+
+    d = Resource.find(resource_id).downloaders.new
+    d.downloader_status      = params['survey']['status'] unless params['survey']['status'].nil?
+    d.participants_country   = params['survey']['participants_country'] unless params['survey']['participants_country'].nil?
+    d.downloader_email       = params['survey']['email'] unless params['survey']['email'].nil?
+    d.save!
+
+    redirect_to '/survey/saved?survey_done=yes&resource_id=' + resource_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + resource_fileset_id.to_s
+  rescue StandardError => e
+    puts e
+    redirect_to '/survey/error'
   end
 end
