44b1e34f6d8d8063a62d94f16cf4ef14ecd072ef
Frank Feng <y.frank.feng@gmail.com>
2019-03-13 16:51:31 +0000

    save downloader info

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -13,11 +13,12 @@ class SurveyController < ApplicationController
     d.downloader_status      = params['survey']['status'] unless params['survey']['status'].nil?
     d.participants_country   = params['survey']['participants_country'] unless params['survey']['participants_country'].nil?
     d.downloader_email       = params['survey']['email'] unless params['survey']['email'].nil?
+    d.ncelp_resource_id      = resource_id
     d.save!
 
     redirect_to '/survey/saved?survey_done=yes&resource_id=' + resource_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + resource_fileset_id.to_s
   rescue StandardError => e
-    puts e
+    puts e.backtrace
     redirect_to '/survey/error'
   end
 end
