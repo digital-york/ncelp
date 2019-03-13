753dd7c45f683303597dadcd963e633d351e4850
Frank Feng <y.frank.feng@gmail.com>
2019-03-13 17:11:06 +0000

    move one line desc before creator

--- app/forms/hyrax/resource_form.rb
+++ app/forms/hyrax/resource_form.rb
@@ -5,9 +5,9 @@ module Hyrax
   class ResourceForm < Hyrax::Forms::WorkForm
     self.model_class = ::Resource
     self.terms = [:title,                # title from default Hyrax metadata
+                  :one_line_description,
                   :creator,              # creator from default Hyrax
                   :affiliation,
-                  :one_line_description,
                   :full_description,
                   :language,             # language from default Hyrax
                   :language_other,
@@ -34,8 +34,8 @@ module Hyrax
 
     self.required_fields = [
                   :title,
-                  :creator,
                   :one_line_description,
+                  :creator,
                   :full_description,
                   :language,
                   :type_of_material,
