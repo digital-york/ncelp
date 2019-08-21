7b57ecf7baeb639a532dd2d5b11972a143f4869b
Frank Feng <y.frank.feng@gmail.com>
2019-08-21 14:28:46 +0000

    only search Resources in HomepageSearchBuilder

--- app/search_builders/hyrax/homepage_search_builder.rb
+++ app/search_builders/hyrax/homepage_search_builder.rb
@@ -1,9 +1,13 @@
 # Added to allow for the My controller to show only things I have edit access to
 class Hyrax::HomepageSearchBuilder < Hyrax::SearchBuilder
   include Hyrax::FilterByType
-  self.default_processor_chain += [:add_access_controls_to_solr_params]
+  self.default_processor_chain += %i[add_access_controls_to_solr_params only_search_resource]
 
   def only_works?
     true
+  end
+
+  def only_search_resource(solr_parameters)
+    solr_parameters[:fq] << "{!field f=has_model_ssim}#{Resource}"
   end
 end
