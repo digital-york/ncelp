b60fb58711eb30c15599b6614730fe9d60c6f58a
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2019-11-26 11:35:36 +0000

    Increas default subcollectin level
    Include level of subcolection to 5 incuding RIs e.g.
    Col 1 <- Col 2 <- Col 3 <- Col 4 <- Resource Items
    
    As documentd at this [FAQ](https://samvera.github.io/collection-nesting-faq.html)

--- config/initializers/samvera-nesting_indexer_initializer.rb
+++ config/initializers/samvera-nesting_indexer_initializer.rb
@@ -5,9 +5,9 @@ require 'hyrax/repository_reindexer'
 
 Samvera::NestingIndexer.configure do |config|
   # How many layers of nesting are allowed for collections
-  # For maximum_nesting_depth of 3 the following will raise an exception
-  # C1 <- C2 <- C3 <- W1
-  config.maximum_nesting_depth = 5
+  # For maximum_nesting_depth of 4 the following will raise an exception
+  # C1 <- C2 <- C3 <- C4 <- W1
+  config.maximum_nesting_depth = 6
   config.adapter = Hyrax::Adapters::NestingIndexAdapter
   config.solr_field_name_for_storing_parent_ids = Solrizer.solr_name('nesting_collection__parent_ids', :symbol)
   config.solr_field_name_for_storing_ancestors =  Solrizer.solr_name('nesting_collection__ancestors', :symbol)
