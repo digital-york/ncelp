3c40c35732707d5154ec40a2434cd886328aae0b
Frank Feng <frank.feng@york.ac.uk>
2020-11-25 17:22:40 +0000

    get first draft version of ReportGeneral and ReportDownloadSurvey

--- app/javascript/components/ReportGeneral.js
+++ app/javascript/components/ReportGeneral.js
@@ -1,10 +1,16 @@
 import React from "react"
+import resource_titles from './json/resource_titles.json'
+import download_total from './json/downloads_total.json'
+import downloads_per_day from './json/downloads_per_day.json'
 
 class ReportGeneral extends React.Component {
+
   render () {
     return (
       <React.Fragment>
-        ReportGeneral
+        Total resources: {Object.keys(resource_titles).length}<br/>
+        Total downloads: {download_total.total}<br/>
+        Downloads over time charts
       </React.Fragment>
     );
   }
