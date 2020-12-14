f2c9507d7266cd56bc2fb82f2375d5dbe8984a05
Frank Feng <frank.feng@york.ac.uk>
2020-12-14 16:23:49 +0000

    no need to save downloader info after user submitting info in the survey form - it will be saved before downloading the resource

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -6,12 +6,6 @@ class SurveyController < ApplicationController
   with_themed_layout 'survey'
 
   def new
-    user_agent = UserAgent.parse(request.user_agent)
-    puts '================'
-    puts user_agent.inspect
-    puts user_agent.browser
-    puts user_agent.platform
-    puts '++++++++++++++++'
   end
 
   def submit
@@ -21,46 +15,32 @@ class SurveyController < ApplicationController
     resource_fileset_id = params['resource_fileset_id']
 
     if from == 'collection'
-      d = Collection.find(collection_id).downloaders.new
       unless params['survey']['status'].nil?
-        d.downloader_status      = params['survey']['status']
         session['survey_status'] = params['survey']['status']
       end
       unless params['survey']['participants_country'].nil?
-        d.participants_country   = params['survey']['participants_country']
         session['participants_country'] = params['survey']['participants_country']
       end
       unless params['survey']['email'].nil?
-        d.downloader_email       = params['survey']['email']
         session['survey_email']  = params['survey']['email']
       end
-      d.collection_id          = collection_id
-      d.save!
-
-      redirect_to '/survey/saved?from='+from+'&survey_done=yes&collection_id=' + collection_id.to_s + '&downloader_id=' + d.id.to_s
+      redirect_to '/survey/saved?from='+from+'&survey_done=yes&collection_id=' + collection_id.to_s
     else
-      d = Resource.find(resource_id).downloaders.new
-      redirect_url = '/survey/saved?from='+from+'&survey_done=yes&resource_id=' + resource_id.to_s + '&downloader_id=' + d.id.to_s + '&fileset_id=' + resource_fileset_id.to_s
+      redirect_url = '/survey/saved?from='+from+'&survey_done=yes&resource_id=' + resource_id.to_s + '&fileset_id=' + resource_fileset_id.to_s
 
       unless params['survey']['status'].blank?
-        d.downloader_status      = params['survey']['status']
         params['survey']['status'].each do |status|
           redirect_url = redirect_url + '&survey_status[]=' + status
         end
       end
       unless params['survey']['participants_country'].blank?
-        d.participants_country   = params['survey']['participants_country']
         params['survey']['participants_country'].each do |c|
           redirect_url = redirect_url + '&participants_country=' + c
         end
       end
       unless params['survey']['email'].blank?
-        d.downloader_email       = params['survey']['email']
         redirect_url = redirect_url + '&survey_email=' + params['survey']['email']
       end
-
-      d.ncelp_resource_id      = resource_id
-      d.save!
 
       redirect_to redirect_url
     end
