4cf13ea93756c6b8bd51722238fb26d4b3915b23
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2021-01-19 17:16:33 +0000

    Add GA trackEvent for Collections download
    Add onclick JS function to push GA _trackEvent():
    - Category -> Collections
    - Action -> Download
    - Label -> Collection ID
    - Value -> Number of resources bundled in zipped collection
    
    Add commented console logging for future testing.

--- app/assets/javascripts/hyrax/ga_events.js
+++ app/assets/javascripts/hyrax/ga_events.js
@@ -5,6 +5,14 @@
 
 $(document).on('click', '#file_download', function(e) {
   _gaq.push(['_trackEvent', 'Files', 'Downloaded', $(this).data('label')]);
+  //console.log(`Hey onclick Files _trackEvent() push ${$(this).data('label')}`)
     
 });
 
+$(document).on('click', '#collection_download_all', function(e) {
+  //data-label - collection resource id
+  //data-value - number of file resources zipped in the collection bundle
+  _gaq.push(['_trackEvent', 'Collections', 'Downloaded', $(this).data('label'), $(this).data('value')]);
+  //console.log(`Hey onclick Collections _trackEvent() push ${$(this).data('label')} including ${$(this).data('int')}`)
+    
+});
