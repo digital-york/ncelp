b49a01b2939cef4c43e07a7f020b261f99c78e5c
Frank Feng <frank.feng@york.ac.uk>
2021-09-16 10:55:25 +0100

    move code into a single js file

---
+++ app/assets/javascripts/tinymce_init.js
@@ -0,0 +1,25 @@
+$(document).on('turbolinks:load', function() {
+    // Hyrax bug: Reinitiate TinyMCE when required
+    var tinyMCEActive = false
+    try {
+        tinyMCEActive = (tinyMCE != 'undefined') && (tinyMCE.activeEditor != null)
+    } catch (error) {}
+    if( tinyMCEActive ) {
+        tinymce.remove();
+        tinyMCE.init({
+            selector: 'textarea.tinymce',
+            height: 500,
+            plugins: 'media link image lists table code visualblocks',
+            menubar: false,
+            toolbar: 'styleselect | formats | undo redo | ' +
+                'bold italic underline strikethrough | ' +
+                'numlist bullist | alignleft aligncenter alignright alignjustify  | ' +
+                'image media link code visualblocks | table tabledelete | ' +
+                'tableprops tablerowprops tablecellprops | ' +
+                'tableinsertrowbefore tableinsertrowafter tabledeleterow | ' +
+                'tableinsertcolbefore tableinsertcolafter tabledeletecol | '
+        })
+    } else {
+        setTimeout(arguments.callee, 50);
+    }
+});
