349ad5852db2d80980114e53c887e8cfaf47c8be
Frank Feng <frank.feng@york.ac.uk>
2021-09-24 12:03:15 +0100

    add more logic to exclude generic URIs, e.g. document: / presentation:

--- app/helpers/string_helper.rb
+++ app/helpers/string_helper.rb
@@ -6,8 +6,14 @@ module StringHelper
     def self.add_link(s)
         updated_s = s.clone
         URI.extract(s).each do |url|
-            unless s.include? "href=\"#{url}\""
-                updated_s.gsub! url, "<a href=\"#{url}\">Link</a>"
+            # Unless Link already with <a> tag
+            # OR
+            # image tag
+            unless updated_s.include? "href=\"#{url}\"" or updated_s.include? "img src=\"#{url}"
+                # only deal with URLs, not generic URIs, e.g. collection: / document:
+                if url.start_with? 'http' or url.start_with? 'https'
+                    updated_s.gsub! url, "<a href=\"#{url}\" target=\"_blank\">Link</a>"
+                end
             end
         end
         updated_s
