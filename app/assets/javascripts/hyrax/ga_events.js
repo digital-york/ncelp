c4babc575f8c3b990ae8f8b0ba362bb5fa9438f2
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2021-09-28 14:48:40 +0100

    bugfix LAN-44 / ga mising data value
    Bugfix do not fire GA event for download survey clicks.

--- app/assets/javascripts/hyrax/ga_events.js
+++ app/assets/javascripts/hyrax/ga_events.js
@@ -51,5 +51,8 @@ $(document).on('click', '#file_download', function(e) {
 $(document).on('click', '#collection_download_all', function(e) {
   //data-label - collection resource id
   //data-value - number of file resources zipped in the collection bundle
-  _gaq.push(['_trackEvent', 'Collections', 'Downloaded', $(this).data('label'), $(this).data('value')]);
+  let data_label = $(this).data('label');
+  if(typeof date_lable !== 'undefined') {
+    _gaq.push(['_trackEvent', 'Collections', 'Downloaded', $(this).data('label'), $(this).data('value')]);
+  }
 });
