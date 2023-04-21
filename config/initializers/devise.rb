5d88c553958c2738f5a589766050854907042584
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2023-04-21 19:51:37 +0100

    Replace enquiries email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'ncelp-enquiries@york.ac.uk'
+  config.mailer_sender = 'ldp-project@york.ac.uk'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
