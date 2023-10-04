243044db164195da5a605121b295f1dd36f4dc73
Sebastian Pałucha <sebastian.palucha@york.ac.uk>
2023-10-04 10:18:09 +0100

    refactor: escape standard error
    Rescue errors
    - file not present
    - JSON parser error

--- app/helpers/download_stats_helper.rb
+++ app/helpers/download_stats_helper.rb
@@ -1,11 +1,19 @@
 module DownloadStatsHelper
   def self.total_downloads
+    # Last GA download count before switching to GA4 account 21/06/2023
+    default_number_of_downloads = '668,578'
     if Figaro.env.download_stats_json.nil?
-      '668,578' # Last GA download count before switching to GA4 account 21/06/2023
+      default_number_of_downloads
     else
-      json_string = File.read(Figaro.env.download_stats_json)
-      # Read number and format it with comma every thousend
-      JSON.parse(json_string)['total_downloads'].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+      begin
+        json_string = File.read(Figaro.env.download_stats_json)
+        # Read number and format it with comma every thousend
+        JSON.parse(json_string)['total_downloads'].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\1,').reverse
+      rescue StandardError => e
+        Rails.logger.error "DownloadStatsHelper.total_downloads => JSON parsing error: #{e.message}"
+        default_number_of_downloads
+      end
+
     end
   end
 end
